2c58dd1606611684a16271d6160533206c6c7187
132_kiyono <132_kiyono@132-kiyono-2.local>
2023-06-22 12:33:42 +0900

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # blackjack_python
-Blackjack code in Python
+This is a blackjack code in Python.  
+It was created according to the rules shown in the following article.  
+https://qiita.com/hirossyi73/items/cf8648c31898216312e5  
+<br>
+  
+- Initial cards are 52. No jokers. No duplicate cards when drawing.
+- Two player game, player and dealer. Player is executor, dealer executes automatically.
+- At the start of the run, the player and the dealer each draw two cards. The cards drawn are displayed on the screen. However, the dealer's second card is not shown.
+- Then the player draws a card first. If the player is over 21, the player bursts, the player loses, and the game ends at that point.
+- Each time a player draws a card, he/she can choose to draw the next card
+- When the player finishes drawing, the dealer then continues drawing until his or her hand is 17 or more
+- When the player and the dealer finish drawing, the game is over. The player who draws closer to 21 wins.
+- J, Q, and K are treated as tens.
+- A is treated only as a "1" for now. No "11".
+- No double-downs, no splits, no surrender, no other special rules
+  
+<br>
+This code is based on the "war" game from "The Self-Taught Programmer" by Cory Althoff.  
+
+You are free to use it. You are free to play and modify it.  
+I am currently studying programming. Please let me know what I can improve.  
---
+++ blackjack.py
@@ -0,0 +1,151 @@
+from random import shuffle
+import sys
+
+class Card:
+
+    suits = ['♥','♠','◆','♣']
+
+    ranks = [None, "A","2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+
+    def __init__(self, s, r):
+        self.suit = s
+        self.rank = r
+
+    def __int__(self):
+        r = self.ranks[self.rank]
+        if r == "J" or r == "Q" or r == "K":
+            r = 10
+        elif r == "A":
+            r = 1
+        return int(r)
+
+    def __str__(self):
+        v = self.ranks[self.rank] + " of " + self.suits[self.suit]
+        return v
+
+class Deck:
+    def __init__(self):
+        self.cards = []
+        for i in range(1, 14):
+            for j in range (4):
+                self.cards.append(Card(j, i))
+        shuffle(self.cards)
+
+    def draw(self):
+        if len(self.cards) == 0:
+            return
+        return self.cards.pop()
+
+class Player():
+    def __init__(self, name):
+        self.total = 0
+        self.name = name
+        self.over = False
+        
+    def match(self):
+        if self.total == 21:
+            s = '{}\'s total score : {} '
+            print(s.format(self.name,self.total))
+            w = 'BLACKJACK! {} win.'
+            print(w.format(self.name))
+            return True
+        elif self.total > 21:
+            s = '{}\'s total score : {} '
+            print(s.format(self.name,self.total))
+            l = 'BUST! {} lose.'
+            print(l.format(self.name,self.total))
+            return True
+        else:
+            return False
+        
+        
+class Game():
+    def __init__(self):
+        self.deck = Deck()
+        self.p = Player('Player')
+        self.d = Player('Dealer')
+        
+    def draw_msg(self,name,card):
+        d = '{} draw {} '
+        print(d.format(name,card))
+
+    def score_msg(self,name,score):
+        s = '{}\'s total score : {} '
+        print(s.format(name,score))
+
+    def hit_stand(self):
+        while True:
+            hs = input("Hit or Stand(h/s):")
+            if hs == "h":
+                return True
+            elif hs == "s":
+                return False
+            else:
+                continue
+                
+    def game_continue(self):
+        while True:
+            yn = input('Continue?(y/n):')
+            if yn == "y":
+                self.p.total = 0
+                self.d.total = 0
+                self.play_game()
+            elif yn == "n":
+                print('Exit game.')
+                sys.exit(-1)
+            else:
+                continue
+
+    def play_game(self):
+        
+        pc1 = self.deck.draw()
+        dc1 = self.deck.draw()
+        pc2 = self.deck.draw()
+        dc2 = self.deck.draw()
+        self.p.total += int(pc1) + int(pc2)
+        self.d.total += int(dc1) + int(dc2)
+
+        self.draw_msg(self.p.name, str(pc1))
+        self.draw_msg(self.p.name, str(pc2))
+        self.draw_msg(self.d.name, str(dc1))
+        self.draw_msg(self.d.name, "(Hole Card)")
+        
+        hs = self.hit_stand()
+
+        while hs:
+           pc = self.deck.draw()
+           self.p.total += int(pc)
+           self.draw_msg(self.p.name, str(pc))
+           if self.p.match():
+               self.game_continue()
+           hs = self.hit_stand()
+            
+        self.score_msg(self.p.name,self.p.total)
+        o = '{}\'s Hole Card : {}'
+        print(o.format(self.d.name,str(dc2)))
+        
+        while self.d.total < 17:
+            dc = self.deck.draw()
+            self.d.total += int(dc)
+            self.draw_msg(self.d.name, str(dc))
+            if self.d.match():
+                self.game_continue()
+                    
+        self.score_msg(self.d.name,self.d.total)
+                
+        if self.d.total < self.p.total:
+            print('{} win.'.format(self.p.name))
+        elif self.d.total > self.p.total:
+            print('{} lose.'.format(self.p.name))
+        else:
+            print('tie.')
+            
+        self.game_continue()
+
+            
+
+
+
+
+game = Game()
+game.play_game()
